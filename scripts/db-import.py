9dae3a87b68165c5a9f900fb872aadd964c2b217
Darren <darren.moreland@icloud.com>
2025-10-29 11:32:46 -0400

    Anonymized database and jupyter scripts

--- scripts/db-import.py
+++ scripts/db-import.py
@@ -1,24 +1,42 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Database connection
-conn_string = 'postgresql://clinops:changeme@postgres/clinops'  
-db = create_engine(conn_string)
+database_db = os.getenv("CLINOPS_DB")
+database_user = os.getenv("CLINOPS_USER")
+database_password = os.getenv("CLINOPS_PASSWORD")
+
+print("DEBUG: database_db = {}".format(database_db))
+print("DEBUG: database_user = {}".format(database_user))
+print("DEBUG: database_password = {}".format(database_password))
+
+conn_string = f"postgresql://{database_user}:{database_password}@postgres/{database_db}"
+
+# db = create_engine(conn_string)
+db = create_engine("postgresql://clinops:changeme@postgres/clinops")
 
 # the folder containing the CSV files
-path = '.'  
+path = "."
 
 # the list of CSV file names
-files = [f for f in os.listdir(path) if f.endswith('.csv')]  
+files = [f for f in os.listdir(path) if f.endswith(".csv")]
 
 # Remove spaces and lowercase file names, then load them into the database
-with db.connect() as conn: 
+with db.connect() as conn:
     for file in files:
         df = pd.read_csv(os.path.join(path, file))
         # Lowercase column names, replace spaces and hyphens with underscores
-        df.columns = [col.lower().replace(' ', '_').replace('-', '_') for col in df.columns]
+        df.columns = [
+            col.lower().replace(" ", "_").replace("-", "_") for col in df.columns
+        ]
         # Generate table name by removing spaces, hyphens and converting to lowercase
-        table_name = "raw_" + file.replace(' ', '_').replace('-', '_').replace('.csv', '').lower()
+        table_name = (
+            "raw_"
+            + file.replace(" ", "_").replace("-", "_").replace(".csv", "").lower()
+        )
         # Load DataFrame into PostgreSQL
-        df.to_sql(table_name, con=conn, if_exists='replace', index=False)
+        df.to_sql(table_name, con=conn, if_exists="replace", index=False)
